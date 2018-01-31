b3da53cf2fe1d1ba9a349a4582af64506f59ec0e
Joachim Nyborg <joachim.c.nyborg@gmail.com>
2018-01-31 16:58:13 +0100

    New environment variable NEWSPAPER_DATA_DIR to enable customization of cache location.

--- newspaper/settings.py
+++ newspaper/settings.py
@@ -32,7 +32,8 @@ NLP_STOPWORDS_EN = os.path.join(
 
 DATA_DIRECTORY = '.newspaper_scraper'
 
-TOP_DIRECTORY = os.path.join(tempfile.gettempdir(), DATA_DIRECTORY) 
+TOP_DIRECTORY = os.environ.get("NEWSPAPER_DATA_DIR") or os.path.join(os.path.expanduser("~"), DATA_DIRECTORY)
+
 if not os.path.exists(TOP_DIRECTORY):
     os.mkdir(TOP_DIRECTORY)
 
